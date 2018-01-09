7b9591312006bedbb4b76df3ca7e0e270ca56148
Igor Shklyar <igor.shklyar@gmail.com>
2018-01-09 10:50:33 +0200

    185 Creating a Fade Transition with the CSS Transition Property

--- src/App.vue
+++ src/App.vue
@@ -26,18 +26,20 @@
 
 <style>
     .fade-enter {
-
+        opacity: 0;
     }
 
   .fade-enter-active {
-
+        transition: opacity 1s;
   }
 
   .fade-leave {
-
+        /*opacity: 1;*/
   }
 
   .fade-leave-active {
+        transition: opacity 1s;
+        opacity: 0;
 
   }
 </style>
