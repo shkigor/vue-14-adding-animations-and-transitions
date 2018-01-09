efb43cdcbd2cbbc748d7d24646d99df9d47b795a
Igor Shklyar <igor.shklyar@gmail.com>
2018-01-09 16:56:42 +0200

    189 Setting Up an Initial on-load Animation

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@
         <transition name="slide" type="animation">
           <div class="alert alert-info" v-if="show">This is some Info</div>
         </transition>
+        <transition name="fade" appear>
+          <div class="alert alert-info" v-if="show">This is some Info</div>
+        </transition>
       </div>
     </div>
   </div>
@@ -21,7 +24,7 @@
   export default {
     data() {
       return {
-          show: false
+          show: true
       }
     }
   }
